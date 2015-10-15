4d9eb0aec9118f33f9ecaa0ed8a748f7b0cdaba7
rizokta <barcelona95@yahoo.com>
2015-10-15 14:20:51 +0700

    Nama : Rizal Oktavian Pradana Kelas : MI 2D NIM  : 1431140135

---
+++ MI 2D_1431140135_rizal/rizal/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Brown Then
+            Me.BackColor = Color.Blue
+        Else
+            Me.BackColor = Color.Brown
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Brown
+    End Sub
+End Class
